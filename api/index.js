7ad4ed6b6adc9cb0cbcc5f81ca708b2ee2cbedee
Michael <mike.alex.martinez@gmail.com>
2018-03-21 21:37:10 +0000

    finished table query

--- api/index.js
+++ api/index.js
@@ -3,27 +3,61 @@
 // Third party modules
 const express = require('express');
 const router = express.Router();
+const Sequelize = require('sequelize');
 
 // db
 const db = require('../db/connect');
+const Op = Sequelize.Op;
 
 // Models
 const { LeagueTables } = require('../db/Models');
 
 router.get('/tables', (req, res) => {
+  /**
+   * filtered tells me whether the data should be filtered
+   * 
+   * If it should be filtered it can be filtered by either home ('h')
+   * away ('a') or total ('t')
+   * 
+   * gameweek will be a number between 1 and 38
+   */
+  console.log(req.query);
+  const { filtered, focus, gameweek } = req.query;
+  const where = {};
+  const defaultSort = [
+    ['gameweek_id', 'ASC'],
+    ['focus', 'DESC'],
+    ['points', 'DESC'],
+    ['goal_difference', 'DESC'],
+    ['for', 'DESC'],
+    ['team_name', 'ASC']
+  ];
 
-  // create filtering for home, away and t.
-  
+  if (filtered === 'true') {
+    if (focus) {
+      where['focus'] = focus;
+    }
+    if (gameweek) {
+      where['gameweek'] = gameweek;
+    }
+  }
 
   // specific gameweeks
-  LeagueTables.findAll()
+  LeagueTables.findAll({
+    where,
+    order: defaultSort
+  })
     .then(items => {
       const tableEntries = items.map(v => v.dataValues);
       res.json({
         status: 'okay',
-        filters: {},
+        filters: where,
         data: tableEntries
       });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json(err);
     });
 
 });
--- db/connect.js
+++ db/connect.js
@@ -2,17 +2,19 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('subootio', process.env.DB_USER, process.env.DB_PWD, {
-  host: 'localhost',
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false,
-  pool: {
-    max: 500,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+const sequelize = new Sequelize('subootio'
+  , process.env.DB_USER
+  , process.env.DB_PWD, {
+    host: 'localhost',
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 500,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
 
 module.exports = sequelize;
